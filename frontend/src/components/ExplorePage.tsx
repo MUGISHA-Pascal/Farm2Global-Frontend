2acf3425e67d701828f10368d293100ab751ae02
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-11-13 11:54:15 +0200

    frontend

--- frontend/src/components/ExplorePage.tsx
+++ frontend/src/components/ExplorePage.tsx
@@ -202,41 +202,47 @@ const ExplorePage = () => {
   }
   return (
     <div className="bg-[#25883F] flex flex-col items-center justify-center w-full border-t-[1px] border-gray-400">
-      <header className="p-[10px] w-full h-[90px] px-[20px] items-center flex flex-row justify-between">
+      <header className="p-[10px] max-md:grid max-md:grid-cols-2 max-md:gap-1 w-full max-md:h-[100px] h-[90px] px-[20px] md:items-center md:flex md:flex-row md:justify-between">
         <RouterLink
           to="/"
-          className="flex flex-row text-white space-x-[10px] items-center "
+          className="flex flex-row text-white space-x-[10px] max-sm:space-x-[5px] items-center "
         >
           <div className="bg-[#4E9A61] p-[6px] hover:border-[1px] border-white rounded-full">
-            <FaArrowLeft className="text-white" />
+            <FaArrowLeft className="text-white max-sm:w-[10px] max-sm:h-[10px] " />
           </div>
-          <p className="text-[14px] font-semibold">back</p>
+          <p className="text-[14px] font-semibold max-sm:text-[10px]">back</p>
         </RouterLink>
-        <h2 className="font-bold text-white text-[22px]">Available farmers</h2>
-        <div className="bg-white pl-[8px]  items-center max-md:hidden relative w-[466px] max-md:w-[200px] h-[42px] rounded-full flex flex-row p-0">
+        <h2 className="font-bold text-white text-[22px] max-sm:text-[17px]">
+          Available farmers
+        </h2>
+        <div className="bg-white pl-[8px]  items-center  relative w-[466px] max-md:w-[350px] h-[42px] rounded-full flex flex-row p-0">
           <select
+            title="select"
             value={searchBy}
             onChange={(e) => {
               setSearchBy(e.target.value);
             }}
             className="w-[110px] focus:outline-none border-[2px] p-[4px] rounded-full border-[#4E9A61] text-gray-500 text-[12px] "
           >
-            <option value="" className="text-[12px] ">
+            <option value="" className="text-[12px] max-md:text-[7px] ">
               search by
             </option>
-            <option value="Name" className="text-[12px] ">
+            <option value="Name" className="text-[12px] max-md:text-[7px]">
               name
             </option>
-            <option value="District" className="text-[12px] ">
+            <option value="District" className="text-[12px] max-md:text-[7px]">
               District
             </option>
-            <option value="Season" className="text-[12px] ">
+            <option value="Season" className="text-[12px] max-md:text-[7px]">
               Season
             </option>
-            <option value="Quality/season" className="text-[12px] ">
+            <option
+              value="Quality/season"
+              className="text-[12px] max-md:text-[7px]"
+            >
               Quality/season
             </option>
-            <option value="Contact" className="text-[12px] ">
+            <option value="Contact" className="text-[12px] max-md:text-[7px]">
               Contact
             </option>
           </select>
@@ -248,7 +254,7 @@ const ExplorePage = () => {
             onChange={(e) => {
               setSearch(e.target.value);
             }}
-            className="focus:outline-none placeholder:text-gray-500 text-gray-600 placeholder:text-[12px] text-[12px] ml-[20px] w-[290px] h-[42px] bg-white"
+            className="focus:outline-none max-sm:w-[190px] placeholder:text-gray-500 text-gray-600 placeholder:text-[12px] text-[12px] max-md:pl-[7px] max-md:ml-0 ml-[20px] w-[290px] h-[42px] bg-white"
           />
           <button
             type="submit"
@@ -259,21 +265,21 @@ const ExplorePage = () => {
           </button>
         </div>
       </header>
-      <main className="w-[1220px] mb-[10px] p-[5px] bg-[#4E9A61] rounded-[20px] ">
-        <main className="farmersMain overflow-y-auto h-screen bg-[#4E9A61] m-[10px] rounded-[20px] w-[1200px] grid grid-cols-4 gap-[20px] p-[30px]">
+      <main className="w-[1220px] max-sm:w-full mb-[10px] p-[5px] bg-[#4E9A61] rounded-[20px] ">
+        <main className="farmersMain overflow-y-auto h-screen bg-[#4E9A61] m-[10px] rounded-[20px] max-md:w-full w-[1200px] grid md:grid-cols-4 max-sm:gap-[5px] max-md:grid-cols-2 max-md:p-[2px] gap-[20px] p-[30px]">
           {filteredFarmers.map((farmer) => (
             <div
               key={farmer.Id}
-              className="flex flex-col h-[290px] w-[250px] bg-[#1B7633]   p-[6px] rounded-[7px] space-y-[10px]"
+              className="flex flex-col h-[290px] max-sm:h-[200px] max-sm:w-[170px] w-[250px] bg-[#1B7633]   p-[6px] rounded-[7px] space-y-[10px]"
             >
               <img
                 src={farmer.image}
                 alt="farmer image"
-                className="rounded-[7px] h-[150px] w-full object-cover bg-cover "
+                className="rounded-[7px] h-[150px] w-full max-sm:h-[90px] object-cover bg-cover "
               />
 
-              <div className="flex flex-row text-[11px] text-white px-[14px] space-x-[10px]">
-                <div className="flex flex-col space-y-[7px] items-start">
+              <div className="flex flex-row text-[11px] max-sm:text-[7px] text-white px-[14px] max-sm:space-x-[14px] space-x-[10px]">
+                <div className="flex flex-col space-y-[7px] max-sm:space-y-[3px] items-start">
                   <b>Farmer Name :</b>
                   <b>District :</b>
                   <b>Harvest Season(s) :</b>
